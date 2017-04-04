3a27e7e081a5427f0df3bfc683bc72c14dcac821
Luke Sadler <lukesadler@MacBook-Pro-3.local>
2017-04-04 21:23:53 +0100

    fixed for apple rejig of older apps

--- sounds/ViewController.m
+++ sounds/ViewController.m
@@ -11,8 +11,6 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 #import "UIColor+CrossFade.h"
-#define SYSTEM_VERSION_LESS_THAN(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-
 
 @interface ViewController ()
 @property (nonatomic) AVAudioPlayer *soundEffectPlayer;
@@ -21,214 +19,173 @@
 
 @implementation ViewController
 
-
-- (void)viewDidLoad
-{
-    noteInt = 0;
-//    chord.alpha = 0;
-    [chordSwitch isOn];
-    
-
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
-    
-    device = [UIDevice currentDevice].model;
-    
-    
-    if (![device hasPrefix:@"iPad"]) {
-    
-        [chordLabel    setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [blueButton    setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [redButton     setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [goldButton    setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [greenButton   setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [greyButton    setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [pinkButton    setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [blackButton   setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [purpleButton  setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [yellowButton  setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [orangeButton  setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [turqButton    setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [maroonButton  setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [chord         setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-        [chordSwitch   setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-
-    }
-    
+- (void)viewDidLoad{
+   [super viewDidLoad];
+   noteInt =-1; //offset so first press is 0
+   
+   [chordSwitch isOn];
+   
+   device = [UIDevice currentDevice].model;
+   
+   if (![device hasPrefix:@"iPad"]) {
+      
+      NSArray * viewArray = @[chordLabel,blueButton,redButton,goldButton,greenButton,greyButton,pinkButton,blackButton,purpleButton,yellowButton,orangeButton,turqButton,maroonButton,chord,chordSwitch];
+      
+      for (UIView * view in viewArray) {
+         [view setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
+      }
+   }
 }
 
 -(IBAction)switchAction:(id)sender{
-    if (chordSwitch.isOn) {
-        chord.alpha = 1;
-    }else{
-        chord.alpha = 0;
-    }
+   if (chordSwitch.isOn) {
+      chord.alpha = 1;
+   }else{
+      chord.alpha = 0;
+   }
 }
 
-
 -(void)colour{
-    
-    CAKeyframeAnimation *animation = nil;
-    CGColorRef finalColor = nil;
-    
-            // Animate to A
-            animation = [UIColor keyframeAnimationForKeyPath:@"backgroundColor"
-                                                    duration:0.5
-                                           betweenFirstColor:self.view.backgroundColor
-                                                   lastColor:self->colorA];
-            finalColor = self->colorA.CGColor;
-
-    self.view.layer.backgroundColor = finalColor;
-    [self.view.layer addAnimation:animation forKey:@"backgroundColorChange"];
-
-    
-    if (colourInt == 1) {
-        chord.textColor = [UIColor whiteColor];
-        chordLabel.textColor = [UIColor whiteColor];
-    }else{
-        chord.textColor = [UIColor blackColor];
-        chordLabel.textColor = [UIColor blackColor];
-    }
-    colourInt = 0;
+   CAKeyframeAnimation *animation = nil;
+   CGColorRef finalColor = nil;
+   
+   // Animate to A
+   animation = [UIColor keyframeAnimationForKeyPath:@"backgroundColor"
+                                           duration:0.5
+                                  betweenFirstColor:self.view.backgroundColor
+                                          lastColor:self->colorA];
+   finalColor = self->colorA.CGColor;
+   
+   self.view.layer.backgroundColor = finalColor;
+   [self.view.layer addAnimation:animation forKey:@"backgroundColorChange"];
+   
+   
+   if (colourInt == 1) {
+      chord.textColor = [UIColor whiteColor];
+      chordLabel.textColor = [UIColor whiteColor];
+   }else{
+      chord.textColor = [UIColor blackColor];
+      chordLabel.textColor = [UIColor blackColor];
+   }
+   colourInt = 0;
 }
 
 
 -(void)music{
-    if (noteInt == 7) {
-        noteInt = 0;
-    }
-    
-    NSString *intString = [NSString stringWithFormat:@"%i%@",noteInt,fileString];
-    
-    NSString *soundPath = [[NSBundle mainBundle] pathForResource:intString ofType:@"aif"];
-    SystemSoundID soundID;
-    AudioServicesCreateSystemSoundID((__bridge CFURLRef)([NSURL fileURLWithPath: soundPath]), &soundID);
-    AudioServicesPlaySystemSound (soundID);
-    
-    }
-
-
+   
+   NSString *intString = [NSString stringWithFormat:@"%i%@",(++noteInt % 7) ,fileString];
+   NSString *soundPath = [[NSBundle mainBundle] pathForResource:intString ofType:@"mp3"];
+   NSURL * url = [NSURL fileURLWithPath:soundPath];
+   
+   AVAudioPlayer * player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
+   [player setVolume:1.0];
+   [player play];
+}
 
 -(IBAction)maroonPressed{
-    fileString = (@"B");
-    colorA =[UIColor colorWithRed:102.0/255.0 green:51.0/255.0 blue:51.0/255.0 alpha:1];
-    colourInt = 1;
-    [self colour];
-    chord.text = @"B";
-    [self music];
-    noteInt ++;
+   fileString = (@"B");
+   colorA =[UIColor colorWithRed:102.0/255.0 green:51.0/255.0 blue:51.0/255.0 alpha:1];
+   colourInt = 1;
+   [self colour];
+   chord.text = @"B";
+   [self music];
 }
 
 -(IBAction)greenPressed{
-    fileString = (@"C");
-    chord.text = @"C";
-    colorA =[UIColor colorWithRed:0.0/255.0 green:153.0/255.0 blue:51.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = (@"C");
+   chord.text = @"C";
+   colorA =[UIColor colorWithRed:0.0/255.0 green:153.0/255.0 blue:51.0/255.0 alpha:1];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)bluePressed{
-    fileString = (@"A");
-    chord.text = @"A";
-    colorA =[UIColor colorWithRed:51.0/255.0 green:102.0/255.0 blue:153.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    noteInt ++;
-    
+   fileString = (@"A");
+   chord.text = @"A";
+   colorA =[UIColor colorWithRed:51.0/255.0 green:102.0/255.0 blue:153.0/255.0 alpha:1];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)turqPressed{
-    fileString = (@"As");
-    colorA =[UIColor colorWithRed:0.0/255.0 green:153.0/255.0 blue:153.0/255.0 alpha:1];
-    chord.text = @"A#";
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = (@"As");
+   colorA =[UIColor colorWithRed:0.0/255.0 green:153.0/255.0 blue:153.0/255.0 alpha:1];
+   chord.text = @"A#";
+   [self colour];
+   [self music];
 }
 
 -(IBAction)blackPressed{
-    colorA = [UIColor blackColor];
-    colourInt = 1;
-    [self colour];
-    chord.text = @"C#";
-    fileString = @"Cs";
-    [self music];
-    noteInt ++;
-    
+   colorA = [UIColor blackColor];
+   colourInt = 1;
+   [self colour];
+   chord.text = @"C#";
+   fileString = @"Cs";
+   [self music];
 }
 
 -(IBAction)redPressed{
-    fileString = @"Ds";
-    colorA =[UIColor colorWithRed:255.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    chord.text = @"D#";
-    noteInt ++;
+   fileString = @"Ds";
+   colorA =[UIColor colorWithRed:255.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1];
+   [self colour];
+   [self music];
+   chord.text = @"D#";
 }
 
 -(IBAction)purplePressed{
-    colourInt = 1;
-    fileString = @"E";
-    chord.text = @"E";
-    colorA =[UIColor colorWithRed:102.0/255.0 green:51.0/255.0 blue:153.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    noteInt ++;
-    
+   colourInt = 1;
+   fileString = @"E";
+   chord.text = @"E";
+   colorA =[UIColor colorWithRed:102.0/255.0 green:51.0/255.0 blue:153.0/255.0 alpha:1];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)pinkPressed{
-    fileString = @"F";
-    chord.text = @"F";
-    colorA =[UIColor colorWithRed:255.0/255.0 green:102.0/255.0 blue:255.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = @"F";
+   chord.text = @"F";
+   colorA =[UIColor colorWithRed:255.0/255.0 green:102.0/255.0 blue:255.0/255.0 alpha:1];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)orangePressed{
-    fileString = @"D";
-    chord.text = @"D";
-//    colorA =[UIColor colorWithRed:255.0/255.0 green:155.0/255.0 blue:51.0/255.0 alpha:1];
-    colorA = [UIColor orangeColor];
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = @"D";
+   chord.text = @"D";
+   //    colorA =[UIColor colorWithRed:255.0/255.0 green:155.0/255.0 blue:51.0/255.0 alpha:1];
+   colorA = [UIColor orangeColor];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)greyPressed{
-    fileString = @"G";
-    chord.text = @"G";
-    colorA =[UIColor lightGrayColor];
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = @"G";
+   chord.text = @"G";
+   colorA =[UIColor lightGrayColor];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)yellowPressed{
-    fileString = @"Fs";
-    chord.text = @"F#";
-    colorA =[UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:0.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = @"Fs";
+   chord.text = @"F#";
+   colorA =[UIColor colorWithRed:255.0/255.0 green:255.0/255.0 blue:0.0/255.0 alpha:1];
+   [self colour];
+   [self music];
 }
 
 -(IBAction)goldPressed{
-    fileString = @"Gs";
-    chord.text = @"G#";
-    colorA =[UIColor colorWithRed:255.0/255.0 green:153.0/255.0 blue:0.0/255.0 alpha:1];
-    [self colour];
-    [self music];
-    noteInt ++;
+   fileString = @"Gs";
+   chord.text = @"G#";
+   colorA =[UIColor colorWithRed:255.0/255.0 green:153.0/255.0 blue:0.0/255.0 alpha:1];
+   [self colour];
+   [self music];
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   [super didReceiveMemoryWarning];
+   // Dispose of any resources that can be recreated.
 }
 
 @end
